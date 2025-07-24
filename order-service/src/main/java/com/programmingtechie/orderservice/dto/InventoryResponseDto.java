a6d56abada6ab0e19c9289617ab8a57c12478c66
Mukesh Reddy <mukeshvancha122@gmail.com>
2025-07-24 12:21:54 -0400

    Webclient in order to check stock

---
+++ order-service/src/main/java/com/programmingtechie/orderservice/dto/InventoryResponseDto.java
@@ -0,0 +1,26 @@
+package com.programmingtechie.orderservice.dto;
+
+public class InventoryResponseDto {
+
+    private String skuCode;
+    private boolean isInStock;
+
+    public boolean isInStock() {
+        return isInStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        isInStock = inStock;
+    }
+
+    public String getSkuCode() {
+        return skuCode;
+    }
+
+    public void setSkuCode(String skuCode) {
+        this.skuCode = skuCode;
+    }
+
+
+
+}
--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.programmingtechie.orderservice.service;
 
+import com.programmingtechie.orderservice.config.WebClientConfig;
+import com.programmingtechie.orderservice.dto.InventoryResponseDto;
 import com.programmingtechie.orderservice.dto.OrderLineItemsDto;
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.model.Order;
@@ -9,20 +11,30 @@ import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 @Transactional
 @Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
+    @Autowired
+    public OrderService(OrderRepository orderRepository, WebClient.Builder webClient) {
+        this.orderRepository = orderRepository;
+        this.webClient = webClient.build();
+    }
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -34,7 +46,23 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+        List<String> skucodes= orderLineItems.stream().map(OrderLineItems::getSkuCode).collect(Collectors.toList());
+
+//        calling the inventory service to check if the items are in stock
+        InventoryResponseDto[] inventoryResponseDtoList= Objects.requireNonNull(webClient.get()
+                .uri("http://localhost:8003/api/v1/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skucodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponseDto[].class)
+                .block());
+
+        boolean productsInStockArray=Arrays.stream(inventoryResponseDtoList).allMatch(InventoryResponseDto::isInStock);
+        if(productsInStockArray){
+            orderRepository.save(order);
+        }
+        else {
+            log.error("Products are not in stock, cannot place order");
+        }
+
 
     }
 
