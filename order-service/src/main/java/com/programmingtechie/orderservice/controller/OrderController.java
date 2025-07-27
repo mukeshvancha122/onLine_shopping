edf817141b14a374975e99b1aec40b555f18c148
Mukesh Reddy <mukeshvancha122@gmail.com>
2025-07-27 16:07:29 -0400

    resilience4j circuit breaker and time-limiting

--- order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.programmingtechie.orderservice.controller;
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.model.Order;
 import com.programmingtechie.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -21,9 +24,15 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+    @CircuitBreaker(name="inventoryService",fallbackMethod = "fallbackPlaceOrder")
+    @TimeLimiter(name = "inventoryService")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
-        return "Order created successfully!";
+        return CompletableFuture.supplyAsync(()->"Order created successfully!");
+    }
+    public CompletableFuture<String> fallbackPlaceOrder(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.info("Exception : "+ runtimeException+"Order creation failed due to inventory service issue"+orderRequest);
+        return CompletableFuture.supplyAsync(() -> "Order creation failed, please try again later.");
     }
 
     @GetMapping("/all")
